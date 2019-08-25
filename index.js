47b5a61a0f910276414186b6813328169631d08c
Ruben Medina <dev@rubenmedina.com>
2019-08-25 00:38:38 -0700

    Simplify promise callbacks

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const FeelsBirthdayMan = require("./FeelsBirthdayMan");
 
 new FeelsBirthdayMan(process.argv.slice(2))
   .postBirthdayMessage()
-  .then(result => console.log(result))
-  .catch(e => console.log(e));
+  .then(console.log)
+  .catch(console.error);
