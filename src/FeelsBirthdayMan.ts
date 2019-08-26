4c25b92acf95c834be96c02561d5231930371f43
Ruben Medina <dev@rubenmedina.com>
2019-08-25 22:28:30 -0700

    Fix merge conflict

--- src/FeelsBirthdayMan.ts
+++ src/FeelsBirthdayMan.ts
@@ -3,11 +3,7 @@ import TranslateRequest from "./TranslateRequest";
 import { SlackUser, BirthdayMessage } from "./types";
 
 class FeelsBirthdayMan {
-<<<<<<< HEAD:src/FeelsBirthdayMan.ts
   private static mentionMatchesUser(mention: string, user: SlackUser): boolean {
-=======
-  static mentionMatchesUser(mention: string, user: SlackUser): boolean {
->>>>>>> 597f303e940298e4e5e9b51b6672bebb78a3e311:src/FeelsBirthdayMan.ts
     return [
       user.profile.display_name_normalized,
       user.profile.display_name,
@@ -38,11 +34,7 @@ class FeelsBirthdayMan {
     };
   }
 
-<<<<<<< HEAD:src/FeelsBirthdayMan.ts
   private async getMentions(usersToMention: string[]): Promise<string> {
-=======
-  async getMentions(usersToMention: string[]): Promise<string> {
->>>>>>> 597f303e940298e4e5e9b51b6672bebb78a3e311:src/FeelsBirthdayMan.ts
     const userList = await this.slackRequest.getSlackUsers();
 
     const mentionBits = this.getMentionBits(usersToMention, userList);
@@ -50,14 +42,10 @@ class FeelsBirthdayMan {
     return mentionBits.length > 0 ? `${mentionBits.join(" ")} ` : "";
   }
 
-<<<<<<< HEAD:src/FeelsBirthdayMan.ts
   private getMentionBits(
     usersToMention: string[],
     userList: SlackUser[],
   ): string[] {
-=======
-  getMentionBits(usersToMention: string[], userList: SlackUser[]): string[] {
->>>>>>> 597f303e940298e4e5e9b51b6672bebb78a3e311:src/FeelsBirthdayMan.ts
     const mentionBits: string[] = [];
 
     usersToMention.forEach(user => {
@@ -77,11 +65,7 @@ class FeelsBirthdayMan {
     return mentionBits;
   }
 
-<<<<<<< HEAD:src/FeelsBirthdayMan.ts
   private async getBirthdayMessage(mentions = ""): Promise<BirthdayMessage> {
-=======
-  async getBirthdayMessage(mentions = ""): Promise<BirthdayMessage> {
->>>>>>> 597f303e940298e4e5e9b51b6672bebb78a3e311:src/FeelsBirthdayMan.ts
     const {
       language,
       translation,
