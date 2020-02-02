fb5df2cef6fcf2d9f5873d6fcfc3a17b25130454
Ruben Medina <dev@rubenmedina.com>
2020-02-01 22:48:31 -0800

    Remove dist

--- dist/TranslateRequest.d.ts
+++
@@ -1,7 +0,0 @@
-import { RandomTranslation } from "./types";
-declare class TranslateRequest {
-  private projectId;
-  constructor(projectId: string);
-  getRandomTranslation(message: string): Promise<RandomTranslation>;
-}
-export default TranslateRequest;
